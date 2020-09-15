caef1c2c1dcef9a6360dd0fe5850d4cd506b44d3
Angardo18 <48691615+Angardo18@users.noreply.github.com>
2020-09-14 22:07:09 -0600

    Proyecto 1
    Proyecto 1 terminado

--- Proyecto1.X/build/default/production/main.o.d
+++ Proyecto1.X/build/default/production/main.o.d
@@ -1,3 +1,3 @@
 build/default/production/main.o:  \
-	C:/Users/AngelOrellana/OneDrive/Escritorio/uvg/4toSemestre/PrograDeMicros/Laboratorios/ProgramacionPic/Proyecto1.X/main.asm  \
+	C:/Users/AngelOrellana/OneDrive/Escritorio/uvg/4toSemestre/PrograDeMicros/Programas_ensamblador/Proyecto1.X/main.asm  \
 
